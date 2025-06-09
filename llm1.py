c8430d5ef4ae4712815355947d8904a844b8d536
ssms27 <ssms27@hotmail.com>
2025-06-08 22:07:18 -0400

    sop

---
+++ llm1.py
@@ -0,0 +1,12 @@
+import urllib.request
+url = ("https://raw.githubusercontent.com/rasbt/" "LLMs-from-Scratch/main/ch02/01_main-chapter-code/" 
+       "the-verdict.txt")
+
+file_path = "the-verdict.txt"
+urllib.request.urlretrieve(url, file_path)
+
+with open(file_path, "r", encoding="utf-8") as f:
+       raw_text = f.read()
+
+print("Total number of characters in the text:", len(raw_text))  #20479
+print(raw_text[:99])
